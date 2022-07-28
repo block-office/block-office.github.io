deaa2a389b6204e93d08a651e1d0369403b2a869
Ethan Goh <ethan@ethangoh.com>
2022-07-28 13:00:31 -0700

    build

--- src/email-validate.ts
+++ src/email-validate.ts
@@ -1,13 +1,12 @@
-"use strict";
-
 var tester =
-  /^[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+  /^[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
 // Thanks to:
 // http://fightingforalostcause.net/misc/2006/compare-email-regex.php
 // http://thedailywtf.com/Articles/Validating_Email_Addresses.aspx
 // http://stackoverflow.com/questions/201323/what-is-the-best-regular-expression-for-validating-email-addresses/201378#201378
 // https://en.wikipedia.org/wiki/Email_address  The format of an email address is local-part@domain, where the
 // local part may be up to 64 octets long and the domain may have a maximum of 255 octets.[4]
+
 export function validateEmail(value: string): boolean {
   if (!value) return false;
 
