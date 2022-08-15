0dcbc6be366a0743e096371e80baf631e78fafd4
Ethan Goh <ethan@ethangoh.com>
2022-08-15 14:59:19 -0700

    Connect cookie consent to GA

--- src/components/cookieConsent.tsx
+++ src/components/cookieConsent.tsx
@@ -1,6 +1,29 @@
-import { CookieConsent as ReactCookieConsent } from "react-cookie-consent";
+import { useEffect } from "react";
+import { CookieConsent as ReactCookieConsent, Cookies, getCookieConsentValue } from "react-cookie-consent";
+import { initGA } from "../googleAnalytics";
+
+const handleDeclineCookie = () => {
+  //remove google analytics cookies
+  Cookies.remove("_ga");
+  Cookies.remove("_gat");
+  Cookies.remove("_gid");
+};
+
+const handleAcceptCookie = () => {
+  const gId = "G-T3PW3BWKJK"; // cspell:disable-line
+  if (gId) {
+    initGA(gId);
+  }
+};
 
 export const CookieConsent = () => {
+  useEffect(() => {
+    const isConsent = getCookieConsentValue();
+    if (isConsent === "true") {
+      handleAcceptCookie();
+    }
+  }, []);
+
   return (
     <ReactCookieConsent
       location="bottom"
@@ -12,6 +35,8 @@ export const CookieConsent = () => {
       declineButtonStyle={{ borderRadius: "2px", padding: "0.5em 1em" }}
       buttonStyle={{ color: "#4e503b", fontSize: "14px", borderRadius: "2px", padding: "0.5em 1em" }}
       expires={150}
+      onAccept={handleAcceptCookie}
+      onDecline={handleDeclineCookie}
     >
       We use cookies to enhance your browsing experience, and analyze our traffic. By clicking "Accept All", you consent
       to our use of cookies.
